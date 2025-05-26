8f58ae915602636f096ce8f1f57f2d5e03d01752
ldgk2712 <ldgk2712@gmail.com>
2025-05-26 22:06:41 +0700

    add

--- app.py
+++ app.py
@@ -73,5 +73,7 @@ def create_plot(A, b, constraint_types, c, objective_type, solution):
     
     return plot_data
 
+import os
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
